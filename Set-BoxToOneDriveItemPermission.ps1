cc1865bcbae0ce111571e78a139575a11948474f
Tadd E. Dawson <tadd@microsoft.com>
2026-03-25 19:20:02 -0400

    Update module requirements in Set-BoxToOneDriveItemPermission.ps1 to remove Microsoft.Online.SharePoint.PowerShell

--- Set-BoxToOneDriveItemPermission.ps1
+++ Set-BoxToOneDriveItemPermission.ps1
@@ -1,4 +1,4 @@
-#Requires -Module Microsoft.Online.SharePoint.PowerShell, PNP.PowerShell
+#Requires -Module PNP.PowerShell
 <#
     .DISCLAIMER
         This Sample Code is provided for the purpose of illustration only and is not intended to be used in a production environment.  
