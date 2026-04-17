3b50e7254db335c9c4d13411fc1f9a9617e9c038
Tadd E. Dawson <tadd@microsoft.com>
2026-04-17 18:11:36 -0400

    Delete obsolete log files for user processing and item permission updates to maintain a clean repository and improve performance.

--- Add-SCA.ps1
+++
@@ -1,7 +0,0 @@
-
-Import-Module Microsoft.Online.SharePoint.PowerShell -Verbose
-
-$SharePointUrl = "https://tenant-admin.sharepoint.com"
-$MigrationAccount = "migrationaccount@tenant.com"
-
-Connect-SPOService -Url $SharePointUrl
--- Get-UserLocalFiles.ps1
+++
@@ -1,7 +0,0 @@
-$UserName = "AdilE"
-$AccountName = ("{0}@{1}.onmicrosoft.com" -f $UserName, "M365CPI19595461")
-$UserContents = Get-ChildItem "C:\Repos\NPSBox\LocalFiles\$AccountName" -Recurse
-
-ForEach ($Item in $UserContents) {
-    Write-Host $Item.FullName
-}
